3e00532ba045de4790f3a5a4c276b5c82f8182da
Tim Allen <tallen@integsys.biz>
2018-12-23 23:36:33 -0500

    ehhh, Diamond is over-complicated

--- impl1/LedTest_impl1_trce.asd
+++ impl1/LedTest_impl1_trce.asd
@@ -1,11 +1,11 @@
 [ActiveSupport TRCE]
 ; Setup Analysis
-Fmax_0 = 75.838 MHz (12.090 MHz);
+Fmax_0 = 47.288 MHz (12.090 MHz);
 Failed = 0 (Total 1);
 Clock_ports = 0;
 Clock_nets = 1;
 ; Hold Analysis
-Fmax_0 = 0.379 ns (0.000 ns);
+Fmax_0 = 0.306 ns (0.000 ns);
 Failed = 0 (Total 1);
 Clock_ports = 0;
 Clock_nets = 1;
--- source/test.v
+++ source/test.v
@@ -6,7 +6,7 @@ module test (
 );
 
 wire	clk;
-reg [20:0] count;
+reg [22:0] count;
 reg [4:0]  step;
 reg [15:0] char_a;
 reg [15:0] char_b;
@@ -26,228 +26,126 @@ begin
 		step = step + 1;
 		if (step == 1)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b0011000011111111; //C
-			*/
-			char_a = 16'b1111111111111110;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
+			char_d = 16'b1100111100000000; //C
 		end
 		if (step == 2)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b0000000011111111; //O
-			*/
-			char_a = 16'b1111111111111101;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 3)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
-			char_d = 16'b0000001101101101; //
-			*/
-			char_a = 16'b1111111111111011;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
+			char_d = 16'b0000001101101101; //O
 		end
 		if (step == 4)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b1111111111010101; //Y
-			*/
-			char_a = 16'b1111111111110111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 5)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b1111111111111111; //
-			*/
-			char_a = 16'b1111111111101111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 6)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b1111111111111111; //
-			*/
-			char_a = 16'b1111111111011111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 
 		if (step == 7)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b1111000110111101; //a
-			*/
-			char_a = 16'b1111111110111111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 8)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b1111110110111101; //n
-			*/
-			char_a = 16'b1111111101111111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 9)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b1100011101111101; //d
-			*/
-			char_a = 16'b1111111011111111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 10)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b1111111111111111; // 
-			*/
-			char_a = 16'b1111110111111111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 11)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b1111111111111111; // 
-			*/
-			char_a = 16'b1111101111111111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 
 		if (step == 12)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b0000110000111111; //A
-			*/	
-			char_a = 16'b1111011111111111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 13)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b0001110000111111; //P
-			*/
-			char_a = 16'b1110111111111111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 14)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b0001110000111111; //P
-			*/
-			char_a = 16'b1101111111111111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 15)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b0000110000111111; //A
-			*/
-			char_a = 16'b1011111111111111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 16)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b1111111111111111; // 
-			*/
-			char_a = 16'b0111111111111111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
 		if (step == 17)
 		begin
-			/*
 			char_a = char_b;
 			char_b = char_c;
 			char_c = char_d;
 			char_d = 16'b1111111111111111; // 
-			*/
-			char_a = 16'b1111111111111111;
-			char_b = char_a;
-			char_c = char_b;
-			char_d = char_c;
 		end
-/*
 		if (step == 18)
 		begin
 			char_a = char_b;
@@ -326,7 +224,6 @@ begin
 			char_c = char_d;
 			char_d = 16'b1111111111111111; // 
 		end
-		*/
 	end //if
 end //always
 
